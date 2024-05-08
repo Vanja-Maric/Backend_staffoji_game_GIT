43d06ac18c9e7961a198fa59d3bd9df2bd517aae
RobertMili <robertmilicevic3869@gmail.com>
2024-05-08 18:44:21 +0200

    add test for user positive, negative, edge case test

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,12 @@
-FROM ubuntu:latest
-LABEL authors="robert"
-
-ENTRYPOINT ["top", "-b"]
+FROM openjdk:17-oracle
+VOLUME /tmp
+ARG DEPENDENCY=target/dependency
+ARG BOOSTAPPDBUSER='user'
+ARG BOOSTAPPDBSECRET='password'
+ENV DATASOURCE=jdbc:sqlserver://boostappdb20212.database.windows.net:1433;database=StepDB;user=Boostapp@boostappdb20212;password={your_password_here};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;
+ENV BOOSTAPPDBUSER=Boostapp
+ENV BOOSTAPPDBSECRET=QwertyBoost123
+COPY ${DEPENDENCY}/BOOT-INF/lib /app/lib
+COPY ${DEPENDENCY}/META-INF /app/META-INF
+COPY ${DEPENDENCY}/BOOT-INF/classes /app
+ENTRYPOINT ["java","-cp","app:app/lib/*","com.example.backend_staffoji_game.BackendStaffojiGameApplication"]
---
+++ src/test/java/com/example/backend_staffoji_game/service/UserServiceTest_IntegrationTest.java
@@ -0,0 +1,106 @@
+package com.example.backend_staffoji_game.service;
+
+import com.example.backend_staffoji_game.dto.UserDto;
+import com.example.backend_staffoji_game.exception.UserAlreadyExistsException;
+import com.example.backend_staffoji_game.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+class UserServiceTest_IntegrationTest {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @BeforeEach
+    public void setUp() {
+        cleanUpDatabase();
+    }
+
+    private void cleanUpDatabase() {
+        userRepository.deleteAll();
+    }
+
+
+    @Test
+    void createUser_edgeCaseTest_checkingToAvoidRaceCondition() {
+        // Check if database is empty
+        assertTrue(databaseIsEmpty());
+
+
+
+        for (int i = 0; i < 100; i++) {
+            // Create a user
+            UserDto userTest = new UserDto("test" + i, "test" + i, "test" + i);
+            // Save user
+            userService.createUser(userTest);
+        }
+
+
+        // Check if user is saved
+        var result = userRepository.findAll();
+
+        // Assert
+        assertEquals(result.size(), 100);
+    }
+
+
+    @Test
+    void createUser_positiveTest() {
+        // Check if database is empty
+        assertTrue(databaseIsEmpty());
+
+        // Create a user
+        UserDto userTest = new UserDto("test", "test", "test");
+
+        // Save user
+        userService.createUser(userTest);
+
+        // Check if user is saved
+        var result = userRepository.findByUsername("test");
+
+        // Assert
+        assertTrue(result.isPresent());
+        assertEquals(result.get().getUsername(), userTest.getUsername());
+        assertEquals(result.get().getPassword(), userTest.getPassword());
+        assertEquals(result.get().getEmail(), userTest.getEmail());
+    }
+
+    @Test
+    void createUser_negativeTest() {
+        // Check if database is empty
+        assertTrue(databaseIsEmpty());
+
+        // Create a user
+        UserDto userTest = new UserDto("testNegative", "testNegative", "testNegative");
+
+        // Save user
+        userService.createUser(userTest);
+
+        // Try to create another user with the same username and email
+        UserDto duplicateUser = new UserDto("testNegative", "testNegative", "testNegative");
+
+        // Assert that a UserAlreadyExistsException is thrown
+        assertThrows(UserAlreadyExistsException.class, () -> userService.createUser(duplicateUser));
+    }
+
+
+    @Test
+    void getAllUsers() {
+    }
+
+
+    private boolean databaseIsEmpty() {
+        return userRepository.findAll().isEmpty();
+    }
+
+}
